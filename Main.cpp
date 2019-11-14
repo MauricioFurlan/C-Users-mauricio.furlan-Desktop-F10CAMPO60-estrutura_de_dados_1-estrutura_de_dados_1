0f187f3ba1c37b2442bb7dcc3ff2d30fbc53f661
Mauricio <mauricio.furlan@hotmail.com>
2019-11-13 23:50:08 -0300

    hard
    esta indo

--- Main.cpp
+++ Main.cpp
@@ -6,43 +6,73 @@
 
 void add_pessoas(std::vector<TAD1>&);
 
+void add_cidade(std::vector<TAD1>&, std::vector<TAD2>&,int);
+
 void printVector(const std::vector<TAD1>&);
 
 void del_pessoa(std::vector<TAD1>&);
 
 void editar_pessoa(std::vector<TAD1>&);
 
+int menu();
+
 int main() {
 
 	std::vector<TAD1> pessoa;
-
-	add_pessoas(pessoa);
-	printVector(pessoa);
-	del_pessoa(pessoa);
-	printVector(pessoa);
-	editar_pessoa(pessoa);
-	printVector(pessoa);
-
-	return 0;
+	std::vector<TAD2> cidade;
+	int index;
+	int op;
+	op = menu();
+	
+	switch (op) {
+	case 1:
+		
+		system("CLS");
+		add_pessoas(pessoa);
+		system("CLS");
+		std::cout << "Jogadores adicionados com louvor!" << "\n";
+		printVector(pessoa);
+		std::cout << "--------------------------------------" << "\n";
+		std::cout << "Digite o index para cadastrar endereco: ";
+		std::cin >> index;
+		system("CLS");
+		add_cidade(pessoa, cidade, index);
+	case 2:
+		system("CLS");
+		printVector(pessoa);
+		break;
+	}
 }
-
 void add_pessoas(std::vector<TAD1>& pessoa) {
 	std::string nome;
-
-	std::cout << "quantos nomes deseja adicionar? ";
+	std::cout << "quantos participantes deseja adicionar? ";
 	int tamanhoNome;
 	std::cin >> tamanhoNome;
 	std::cin.ignore();
 	for (int i = 0; i < tamanhoNome; i++) {
 		std::cout << "nome: ";
-		
 		getline(std::cin, nome);
-
 		TAD1 novoNome(nome);
 		pessoa.push_back(novoNome);
 		std::cout << "\n";
 	}
+
 	std::cout << "\n";
+}
+void add_cidade(std::vector<TAD1>& pessoa ,std::vector<TAD2>& cidades, int index) {
+	std::string city;
+	int qntcity;
+	std::cout << "Quantas cidades o participante irá representar?";
+	std::cin >> qntcity;
+	std::cin.ignore();
+	for (int i = 0; i < qntcity; i++) {
+		std::cout << "Cidade: ";
+		getline(std::cin, city);
+		TAD2 novaCity(city);
+		cidades.push_back(novaCity);
+		pessoa[index].push_back(cidades);
+		std::cout << "\n";
+	}
 }
 void printVector(const std::vector<TAD1>& pessoa) {
 	size_t size = pessoa.size();
@@ -66,4 +96,17 @@ void editar_pessoa(std::vector<TAD1>& pessoa) {
 	std::cout << "Digite o novo nome: ";
 	std::cin >> novoEdit;
 	pessoa.at(op) = novoEdit;
+}
+
+int menu() {
+	unsigned int op;
+	std::cout << "-------------" << "\n";
+	std::cout << "Liga Pokemon" << "\n";
+	std::cout << "-------------" << "\n";
+	std::cout << "[1] Adicionar Competidor" << "\n";
+	std::cout << "[2] Listar Competidores" << "\n";
+	std::cout << "-------------" << "\n";
+	std::cout << "Op: ";
+	std::cin >> op;
+	return op;
 }
