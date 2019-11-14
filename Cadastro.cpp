67467c24fd9a374450acc74bdcc35cf388da5b18
sankamau <48259868+sankamau@users.noreply.github.com>
2019-11-14 17:16:16 -0300

    indo
    pra frnte

--- Cadastro.cpp
+++ Cadastro.cpp
@@ -1,4 +1,3 @@
-#include <string>
 #include "Cadastro.h"
 
 TAD1::TAD1() {
@@ -15,38 +14,4 @@ std::string TAD1::getName() const {
 }
 void TAD1::setName(std::string nome) {
 	novoNome = nome;
-}
-
-TAD2::TAD2()  {
-	//novoNome = "";
-}
-TAD2::TAD2(std::string cidade) {
-	novaCidade = cidade;
-}
-TAD2::~TAD2() {
-
-}
-std::string TAD2::getCidade() const {
-	return novaCidade;
-}
-
-void TAD2::setCidade(std::string cidade){
-	novaCidade = cidade;
-}
-
-TAD3::TAD3() {
-	//novoNome = "";
-}
-TAD3::TAD3(std::string pokemon) {
-	novoPokemon = pokemon;
-}
-TAD3::~TAD3() {
-
-}
-std::string TAD3::getPokemon() const {
-	return novoPokemon;
-}
-
-void TAD3::setPokemon(std::string pokemon){
-	novoPokemon = pokemon;
 }
--- Cadastro.h
+++ Cadastro.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <string>
 
@@ -15,39 +14,8 @@ public:
 	// acesso a funcao
 	std::string getName() const;
 	void setName(std::string); 
+
+
 private:
 	std::string novoNome;
-};
-
-class TAD2 {
-public:
-	//construtor
-	TAD2();
-	//Overload construtor
-	TAD2(std::string);
-	// destructor
-	~TAD2();
-	// acesso a funcao
-	std::string getCidade() const;
-	void setCidade(std::string);
-
-private:
-	std::string novaCidade;
-};
-
-
-class TAD3 {
-public:
-	//construtor
-	TAD3();
-	//Overload construtor
-	TAD3(std::string);
-	// destructor
-	~TAD3();
-	// acesso a funcao
-	std::string getPokemon() const;
-	void setPokemon(std::string);
-
-private:
-	std::string novoPokemon;
 };
